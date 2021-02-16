684ed5637d05f413da94f9e6cc9abc964fd84753
sorrick <63976854+sorrick@users.noreply.github.com>
2021-02-16 10:00:36 -0600

    All files for the Mini Calc application

---
+++ Mini-Calc/Mini-Calc/frmMiniCalc.Designer.vb
@@ -0,0 +1,164 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMiniCalc
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtValue1 = New System.Windows.Forms.TextBox()
+        Me.txtValue2 = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnsubtract = New System.Windows.Forms.Button()
+        Me.btnmultiply = New System.Windows.Forms.Button()
+        Me.btndivide = New System.Windows.Forms.Button()
+        Me.lbTitle = New System.Windows.Forms.Label()
+        Me.txtResult = New System.Windows.Forms.TextBox()
+        Me.btnAbout = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtValue1
+        '
+        Me.txtValue1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtValue1.Location = New System.Drawing.Point(51, 80)
+        Me.txtValue1.Name = "txtValue1"
+        Me.txtValue1.Size = New System.Drawing.Size(100, 31)
+        Me.txtValue1.TabIndex = 0
+        '
+        'txtValue2
+        '
+        Me.txtValue2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtValue2.Location = New System.Drawing.Point(51, 137)
+        Me.txtValue2.Name = "txtValue2"
+        Me.txtValue2.Size = New System.Drawing.Size(100, 31)
+        Me.txtValue2.TabIndex = 1
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.Location = New System.Drawing.Point(212, 76)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(48, 38)
+        Me.btnAdd.TabIndex = 2
+        Me.btnAdd.Text = "+"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnsubtract
+        '
+        Me.btnsubtract.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnsubtract.Location = New System.Drawing.Point(212, 133)
+        Me.btnsubtract.Name = "btnsubtract"
+        Me.btnsubtract.Size = New System.Drawing.Size(48, 38)
+        Me.btnsubtract.TabIndex = 3
+        Me.btnsubtract.Text = "-"
+        Me.btnsubtract.UseVisualStyleBackColor = True
+        '
+        'btnmultiply
+        '
+        Me.btnmultiply.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnmultiply.Location = New System.Drawing.Point(286, 76)
+        Me.btnmultiply.Name = "btnmultiply"
+        Me.btnmultiply.Size = New System.Drawing.Size(48, 38)
+        Me.btnmultiply.TabIndex = 4
+        Me.btnmultiply.Text = "X"
+        Me.btnmultiply.UseVisualStyleBackColor = True
+        '
+        'btndivide
+        '
+        Me.btndivide.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btndivide.Location = New System.Drawing.Point(286, 133)
+        Me.btndivide.Name = "btndivide"
+        Me.btndivide.Size = New System.Drawing.Size(48, 38)
+        Me.btndivide.TabIndex = 5
+        Me.btndivide.Text = "/"
+        Me.btndivide.UseVisualStyleBackColor = True
+        '
+        'lbTitle
+        '
+        Me.lbTitle.AutoSize = True
+        Me.lbTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbTitle.Location = New System.Drawing.Point(86, 22)
+        Me.lbTitle.Name = "lbTitle"
+        Me.lbTitle.Size = New System.Drawing.Size(224, 33)
+        Me.lbTitle.TabIndex = 6
+        Me.lbTitle.Text = "Mini Calculator"
+        '
+        'txtResult
+        '
+        Me.txtResult.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtResult.Location = New System.Drawing.Point(51, 207)
+        Me.txtResult.Multiline = True
+        Me.txtResult.Name = "txtResult"
+        Me.txtResult.ReadOnly = True
+        Me.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtResult.Size = New System.Drawing.Size(283, 150)
+        Me.txtResult.TabIndex = 7
+        '
+        'btnAbout
+        '
+        Me.btnAbout.Location = New System.Drawing.Point(76, 374)
+        Me.btnAbout.Name = "btnAbout"
+        Me.btnAbout.Size = New System.Drawing.Size(75, 23)
+        Me.btnAbout.TabIndex = 8
+        Me.btnAbout.Text = "About"
+        Me.btnAbout.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(235, 374)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 9
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'frmMiniCalc
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(388, 450)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnAbout)
+        Me.Controls.Add(Me.txtResult)
+        Me.Controls.Add(Me.lbTitle)
+        Me.Controls.Add(Me.btndivide)
+        Me.Controls.Add(Me.btnmultiply)
+        Me.Controls.Add(Me.btnsubtract)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtValue2)
+        Me.Controls.Add(Me.txtValue1)
+        Me.Name = "frmMiniCalc"
+        Me.Text = "Mini Calculator(Richard Soria #35)"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtValue1 As TextBox
+    Friend WithEvents txtValue2 As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnsubtract As Button
+    Friend WithEvents btnmultiply As Button
+    Friend WithEvents btndivide As Button
+    Friend WithEvents lbTitle As Label
+    Friend WithEvents txtResult As TextBox
+    Friend WithEvents btnAbout As Button
+    Friend WithEvents btnClear As Button
+End Class
---
+++ Mini-Calc/Mini-Calc/frmMiniCalc.vb
@@ -0,0 +1,80 @@
+﻿Public Class frmMiniCalc
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtValue1.Text = ""
+        txtValue2.Text = ""
+        txtResult.Text = ""
+        txtValue1.Select()
+
+    End Sub
+
+    Private Sub frmMiniCalc_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles txtResult.TextChanged
+
+    End Sub
+
+    Private Sub btnsubtract_Click(sender As Object, e As EventArgs) Handles btnsubtract.Click
+        Dim value1 As Double
+        Dim value2 As Double
+        Dim result As Double
+
+        value1 = txtValue1.Text
+
+        value2 = txtValue2.Text
+
+        result = value1 - value2
+
+        txtResult.Text = value1 & " minus " & value2 & " equals " & result & Environment.NewLine & txtResult.Text
+    End Sub
+
+    Private Sub btnAbout_Click(sender As Object, e As EventArgs) Handles btnAbout.Click
+        MessageBox.Show("Mini Calculator" & Environment.NewLine & "(c) 2019 College of Dupage" & Environment.NewLine & "Allrights Reserved")
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim value1 As Double
+        Dim value2 As Double
+        Dim result As Double
+
+        value1 = txtValue1.Text
+
+        value2 = txtValue2.Text
+
+        result = value1 + value2
+
+        txtResult.Text = value1 & " plus " & value2 & " equals " & result & Environment.NewLine & txtResult.Text
+
+    End Sub
+
+    Private Sub btnmultiply_Click(sender As Object, e As EventArgs) Handles btnmultiply.Click
+        Dim value1 As Double
+        Dim value2 As Double
+        Dim result As Double
+
+        value1 = txtValue1.Text
+
+        value2 = txtValue2.Text
+
+        result = value1 * value2
+
+        txtResult.Text = value1 & " multiplied " & value2 & " equals " & result & Environment.NewLine & txtResult.Text
+    End Sub
+
+    Private Sub btndivide_Click(sender As Object, e As EventArgs) Handles btndivide.Click
+        Dim value1 As Double
+        Dim value2 As Double
+        Dim result As Double
+
+        value1 = txtValue1.Text
+
+        value2 = txtValue2.Text
+
+        result = value1 / value2
+
+        txtResult.Text = value1 & " divided by " & value2 & " equals " & result & Environment.NewLine & txtResult.Text
+    End Sub
+
+
+End Class
